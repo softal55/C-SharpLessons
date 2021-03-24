6c161ed90dec0e8ee5e5cf427abb8f7b21a8b985
Sofiane taleb <str14.don@gmail.com>
2021-03-24 17:11:18 +0100

    The new project

--- C-SharpLessons/Program.cs
+++ C-SharpLessons/Program.cs
@@ -6,7 +6,7 @@ namespace C_SharpLessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
