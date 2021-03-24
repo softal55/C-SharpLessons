aebf4436b5b7e113f65a1ad08e8df66170b55afe
Sofiane taleb <str14.don@gmail.com>
2021-03-24 17:10:26 +0100

    Add project files.

---
+++ C-SharpLessons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_SharpLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
